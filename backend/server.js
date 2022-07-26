e748910dda59d9e29530b4c2f11d0e1c2d7b4d0f
Jacob Lopez <jacoblopez.dev@gmail.com>
2022-07-25 20:45:33 -0500

    Heroku Bug fix #2
    build folder bug fix

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ if(process.env.NODE_ENV === 'production'){
     // Set static build folder
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+    app.get('*', (req, res) => res.sendFile(__dirname, '../frontend/build/index.html'))
 }else{
     app.get('/', (req,res) => {
         res.status(201).json({message: 'Welcome to Crypto Top 10'})
