7f50b8bbb38e8105d8027571cc7ffb1729e5eef6
Jacob Lopez <jacoblopez.dev@gmail.com>
2022-07-25 22:06:46 -0500

    heroku bug fix #3
    App crashes on refresh. Heroku is attempting to make another call when it should just be react re-rendering the component.
    - additional changes to path.

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,9 @@ if(process.env.NODE_ENV === 'production'){
     // Set static build folder
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(__dirname, '../frontend/build/index.html'))
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+      })
 }else{
     app.get('/', (req,res) => {
         res.status(201).json({message: 'Welcome to Crypto Top 10'})
